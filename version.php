8815ed6d0e6ef4e6d3891e40206c5a0ece871ea2
Patrick Thibaudeau <thibaud@yorku.ca>
2022-11-29 15:20:23 -0500

    Added Bootstrap 4.x style sheet to editor. Important for TinyMCE

--- version.php
+++ version.php
@@ -30,7 +30,7 @@ defined('MOODLE_INTERNAL') || die();
 $plugin->component = 'theme_moove';
 
 // This is the version of the plugin.
-$plugin->version = 2022062007;
+$plugin->version = 2022112900;
 
 // This is the named version.
 $plugin->release = '4.0.7';
