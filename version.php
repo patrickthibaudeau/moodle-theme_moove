42f9c9c46d1d458edf4ec3bab0d1b49ac631beb2
Patrick Thibaudeau <thibaud@yorku.ca>
2024-01-13 06:34:11 -0500

    Calendar pop-up adjusted to be above secondary nav bar.

--- version.php
+++ version.php
@@ -30,7 +30,7 @@ defined('MOODLE_INTERNAL') || die();
 $plugin->component = 'theme_moove';
 
 // This is the version of the plugin.
-$plugin->version = 20230071300;
+$plugin->version = 2024011300;
 
 // This is the named version.
 $plugin->release = '4.1.2';
