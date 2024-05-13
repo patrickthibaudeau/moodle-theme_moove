76ddfa39f7bb1ccf09283d4e89eb40fe4a012df6
Patrick Thibaudeau <thibaud@yorku.ca>
2024-05-13 07:48:50 -0400

    Fixed login container width.

--- version.php
+++ version.php
@@ -30,7 +30,7 @@ defined('MOODLE_INTERNAL') || die();
 $plugin->component = 'theme_moove';
 
 // This is the version of the plugin.
-$plugin->version = 2024051200;
+$plugin->version = 2024051300;
 
 // This is the named version.
 $plugin->release = '4.5.0';
