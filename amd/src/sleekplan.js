856bcc3c5599ccc7b7a362cd243c252b3860798a
Patrick Thibaudeau <thibaud@yorku.ca>
2024-07-04 10:31:29 -0400

    YULRN-541 - Removed user menu options.

--- amd/src/sleekplan.js
+++ amd/src/sleekplan.js
@@ -5,22 +5,21 @@ define(['jquery', 'core/log', 'core/config'], function ($, log, mdlcfg) {
 
     log.debug('Add sleekplan initialised');
 
-    function init_sleekplan(){
-      var urlRemote = mdlcfg.wwwroot + "/local/yorktasks/sleekplantoken.php"
-      log.debug('Generating Sleekplan SSO token');
-      $.post(urlRemote,
-      {
-      }, function(data, status){
-          if (status == "success"){
-              log.debug('Sleekplan token generated, adding Sleekplan');
-              window.SLEEK_USER = {
-                token: data,
-              };
-              var sleekplanid = $('#sleekplanid').val();
-              var sleekplanhtml = "<script type='text/javascript'>window.$sleek=[];window.SLEEK_PRODUCT_ID=" + sleekplanid + ";(function(){d=document;s=d.createElement('script');s.src='https://client.sleekplan.com/sdk/e.js';s.async=1;d.getElementsByTagName('head')[0].appendChild(s);})();</script>";
-              $("body").append(sleekplanhtml);
-          }
-      });
+    function init_sleekplan() {
+        var urlRemote = mdlcfg.wwwroot + "/local/yorktasks/sleekplantoken.php"
+        log.debug('Generating Sleekplan SSO token');
+        $.post(urlRemote,
+            {}, function (data, status) {
+                if (status == "success") {
+                    log.debug('Sleekplan token generated, adding Sleekplan');
+                    window.SLEEK_USER = {
+                        token: data,
+                    };
+                    var sleekplanid = $('#sleekplanid').val();
+                    var sleekplanhtml = "<script type='text/javascript'>window.$sleek=[];window.SLEEK_PRODUCT_ID=" + sleekplanid + ";(function(){d=document;s=d.createElement('script');s.src='https://client.sleekplan.com/sdk/e.js';s.async=1;d.getElementsByTagName('head')[0].appendChild(s);})();</script>";
+                    $("body").append(sleekplanhtml);
+                }
+            });
     }
 
     return {
